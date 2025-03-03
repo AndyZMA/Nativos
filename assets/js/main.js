8efe6fd1029531e56af647d120d156643d0e7aae
andyZMA <andreszma75@gmail.com>
2025-03-02 22:16:29 -0600

    routes

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+
+    // Al cargar el DOM, la imagen de fondo se hace visible con un efecto fade-in
+    document.addEventListener("DOMContentLoaded", function () {
+      document.querySelector(".background-image").classList.add("loaded");
+    });
